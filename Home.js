dc6fd134ad81dc5936115f6f06ec8103680bdf04
azizajmalkhan <126547245+azizajmalkhan@users.noreply.github.com>
2025-05-25 18:31:03 +0530

    Add files via upload

---
+++ Home.js
@@ -0,0 +1,45 @@
+import React, { useContext } from "react";
+import noteContext from "../context/notes/noteContext";
+
+
+const Home = () => {
+    const context = useContext(noteContext)
+    const { notes, setNotes } = context
+    return (
+        <div>
+            <form>
+                <div className="mb-3">
+                    <label for="exampleInputEmail1" className="form-label">Email address</label>
+                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"></input>
+                    <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+                </div>
+                <div className="mb-3">
+                    <label for="exampleInputPassword1" className="form-label">Password</label>
+                    <input type="password" className="form-control" id="exampleInputPassword1"></input>
+                </div>
+                <div className="mb-3 form-check">
+                    <input type="checkbox" className="form-check-input" id="exampleCheck1"></input>
+                    <label className="form-check-label" for="exampleCheck1">Check me out</label>
+                </div>
+                <button type="submit" className="btn btn-primary">Submit</button>
+            </form>
+            <div>
+                <h2>You Notes</h2>
+                {notes.map((note) =>
+                //  (
+                //     <div key={note._id}>
+                //         <h3>{note.title}</h3>
+                //         <p>{note.description}</p>
+                //     </div>
+                // )
+            {
+                return note.title
+            }
+                )}
+
+            </div>
+        </div>
+    )
+}
+
+export default Home
